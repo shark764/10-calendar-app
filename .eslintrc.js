c76a0e4a6071e1b779c23e8603ae98c5ccc7e470
Farid Hernandez <farid.hdz.64@gmail.com>
2022-06-23 10:12:59 -0600

    chore: Improving eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -57,6 +57,9 @@ config.rules = {
       trailingUnderscore: 'allow',
     },
   ],
+  // note you must disable the base rule as it can report incorrect errors
+  'no-unused-vars': 'off',
+  '@typescript-eslint/no-unused-vars': ['warn', { vars: 'all', args: 'all' }],
 };
 
 module.exports = config;
